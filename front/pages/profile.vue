4936d8dca187adaa021484e3112f5308d6717121
Олег Сайфутдинов <zeusfly@mail.ru>
2023-07-29 07:04:29 +0300

    Med experiments

--- front/pages/profile.vue
+++ front/pages/profile.vue
@@ -1,15 +1,28 @@
 <template>
   <div>
+    <VTextField
+      v-model="inn"
+    />
+    <v-btn
+      @click="testSbis"
+    >
+      Test
+    </v-btn>
   </div>
 </template>
 
 <script setup>
+import {getSbis} from "~/composables/globals";
 useHead({title: 'Личный кабинет'});
-definePageMeta({
-  middleware: 'auth'
-});
+definePageMeta({ middleware: 'auth' });
+// '7721175899'
 
+const inn = ref('');
 
+const testSbis = async () => {
+  const { data, error } = await useFetch('https://api.sbis.ru/vok-demo/req?inn=' + inn.value);
+  console.log(JSON.stringify(data))
+}
 </script>
 
 <style lang="scss" scoped>

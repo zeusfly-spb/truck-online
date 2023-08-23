719362b5cd64222015635664d50ba79c365e23d7
Олег Сайфутдинов <zeusfly@mail.ru>
2023-08-23 13:23:27 +0300

    Some fixes

--- front/app.vue
+++ front/app.vue
@@ -1,16 +1,8 @@
 <template>
   <div>
     <NuxtLayout>
-      <NuxtLoadingIndicator />
-      <NuxtPage />
+      <NuxtLoadingIndicator/>
+      <NuxtPage/>
     </NuxtLayout>
   </div>
 </template>
-<script setup lang="js">
-import { useAuthStore } from "~/store/auth";
-const authStore = useAuthStore();
-const authenticated = computed(() => authStore.authenticated);
-const token_cookie = useCookie('online_port_token');
-token_cookie.value ? await authStore.getUserDetails() : null;
-watch(authenticated, async val => val ? await navigateTo('/profile') : await navigateTo('/login'));
-</script>

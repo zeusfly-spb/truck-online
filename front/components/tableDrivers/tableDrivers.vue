6dc4751de21ca7cf2f5ee7ed162e7c426c51b3d2
Артем Чмиль <artem.chmil@bk.ru>
2023-09-26 13:31:46 +0300

    routine

---
+++ front/components/tableDrivers/tableDrivers.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+<script setup>
+import { useDriversStore } from "~/store/drivers";
+const driverStore = useDriversStore();
+
+const allDrivers = computed(() => {
+  if (!driverStore.drivers || driverStore.loading) return [];
+  return driverStore.drivers;
+});
+</script>
+<style lang=""></style>

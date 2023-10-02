ef7ff051fb023d2143b986919af836aca3a9f979
gurbanmyradownaguljahan@gmail.com <gurbanmyradownaguljahan@gmail.com>
2023-10-03 01:36:32 +0500

    added some admin lists

---
+++ front/components/admin/Sidebar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-navigation-drawer app>
+      <v-list>
+        <v-list-item-group>
+          <v-list-item to="/admin">
+            <v-list-item-content>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/admin/users">
+            <v-list-item-content>
+              <v-list-item-title>Users</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/admin/drivers">
+            <v-list-item-content>
+              <v-list-item-title>Drivers</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/admin/addresses">
+            <v-list-item-content>
+              <v-list-item-title>Addresses</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+</template>
---
+++ front/pages/admin/index.vue
@@ -0,0 +1,14 @@
+<!-- index.vue -->
+<template>
+  <Sidebar />
+</template>
+<script>
+import Sidebar from "~/components/admin/Sidebar.vue";
+
+export default {
+  components: {
+    Sidebar,
+  },
+  // Other layout/page logic goes here
+};
+</script>
